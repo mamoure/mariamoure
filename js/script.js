1192c1a42dd4a827046bbd3a2b09e4316c3548bc
Maria <maria.moure.rosende@gmail.com>
2026-07-06 19:32:13 +0200

    act

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const proyectos = [
   {
     codigo: "20.",
-    coord: "00.0000000, 00.0000000",
+    coord: "42.8782, -8.5448 ",
     nombre: "Compostela Xadrez",
     servicios: "Plataforma de datos",
     toolbox: "Cartografía, Desarrollo web",
@@ -10,7 +10,7 @@ const proyectos = [
   },
   {
     codigo: "19.",
-    coord: "00.0000000, 00.0000000",
+    coord: "41.3500, 2.0500",
     nombre: "Agenda Urbana del Baix Llobregat (AUBL)",
     servicios: "Evaluación de políticas, Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
@@ -19,7 +19,7 @@ const proyectos = [
   },
   {
     codigo: "18.",
-    coord: "00.0000000, 00.0000000",
+    coord: "39.1653, -0.2559",
     nombre: "Plan de Actuación Integrado Cullera",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
@@ -28,7 +28,7 @@ const proyectos = [
   },
   {
     codigo: "17.",
-    coord: "00.0000000, 00.0000000",
+    coord: "39.4913, -0.4635",
     nombre: "Plan de Actuación Integrado Manises",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
@@ -37,16 +37,16 @@ const proyectos = [
   },
   {
     codigo: "16.",
-    coord: "00.0000000, 00.0000000",
+    coord: "41.4890, 2.3570",
     nombre: "Actualización Plan de Acción de Premià de Mar",
     servicios: "Evaluación de políticas, Diagnóstico Integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Ajuntament de Premiá de Mar, TECHfriendly",
-    img: ""
+    img: "img/16.png"
   },
   {
     codigo: "15.",
-    coord: "00.0000000, 00.0000000",
+    coord: "41.3436, 2.0419",
     nombre: "Pla de Barris Sant Boi de Llobregat",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
@@ -55,16 +55,16 @@ const proyectos = [
   },
   {
     codigo: "14.",
-    coord: "00.0000000, 00.0000000",
+    coord: "39.1930, -0.4360",
     nombre: "Agenda Urbana de Reconstrucción de Algemesí",
     servicios: "Evaluación de políticas, Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Ajuntament de Algemesí, TECHfriendly",
-    img: ""
+    img: "img/14.png"
   },
   {
     codigo: "13.",
-    coord: "00.0000000, 00.0000000",
+    coord: "43.5560, -5.9248",
     nombre: "Plan de Actuación Integrado Avilés",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
@@ -73,7 +73,7 @@ const proyectos = [
   },
   {
     codigo: "12.",
-    coord: "00.0000000, 00.0000000",
+    coord: "41.4926, 2.0312",
     nombre: "Plan de Actuación Integrado Rubí",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
@@ -82,7 +82,7 @@ const proyectos = [
   },
   {
     codigo: "11.",
-    coord: "00.0000000, 00.0000000",
+    coord: "41.3030, 2.0012",
     nombre: "Plan de Actuación Integrado Gavà",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
@@ -91,7 +91,7 @@ const proyectos = [
   },
   {
     codigo: "10.",
-    coord: "00.0000000, 00.0000000",
+    coord: "42.5618, -8.9915",
     nombre: "Plan de Actuación Integrado Ribeira",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
@@ -100,16 +100,16 @@ const proyectos = [
   },
   {
     codigo: "09.",
-    coord: "00.0000000, 00.0000000",
+    coord: "6.7213, -4.4214",
     nombre: "Actualización Diagnóstico AU Málaga",
     servicios: "Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Ayuntamiento de Málaga, TECHfriendly",
-    img: ""
+    img: "img/09.gif"
   },
   {
     codigo: "08.",
-    coord: "00.0000000, 00.0000000",
+    coord: "41.3500, 2.0500",
     nombre: "PAC Baix Llobregat 2024-2027",
     servicios: "Evaluación de políticas, Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
@@ -118,7 +118,7 @@ const proyectos = [
   },
   {
     codigo: "07.",
-    coord: "00.0000000, 00.0000000",
+    coord: "41.4500, 2.2474",
     nombre: "Badalona Estrategia 2030",
     servicios: "Evaluación de políticas, Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
@@ -127,16 +127,16 @@ const proyectos = [
   },
   {
     codigo: "06.",
-    coord: "00.0000000, 00.0000000",
+    coord: "41.3500, 2.0500",
     nombre: "Estudio turismo azul costa Barcelona",
     servicios: "Evaluación de políticas, Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Diputació de Barcelona, TECHfriendly",
-    img: "img/13.jpg"
+    img: "img/06.gif"
   },
   {
     codigo: "05.",
-    coord: "00.0000000, 00.0000000",
+    coord: "42.5126, -8.8129",
     nombre: "Palimpsesto Atlántico",
     servicios: "Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
@@ -145,7 +145,7 @@ const proyectos = [
   },
   {
     codigo: "04.",
-    coord: "00.0000000, 00.0000000",
+    coord: "41.3851, 2.1734",
     nombre: "Análisis de la conectividad ecológica",
     servicios: "Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
@@ -154,7 +154,7 @@ const proyectos = [
   },
   {
     codigo: "03.",
-    coord: "00.0000000, 00.0000000",
+    coord: "41.3851, 2.1734",
     nombre: "Prioridades Invertidas",
     servicios: "Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
@@ -163,7 +163,7 @@ const proyectos = [
   },
   {
     codigo: "02.",
-    coord: "00.0000000, 00.0000000",
+    coord: "42.7241, -7.6833",
     nombre: "Gestión del agua",
     servicios: "Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
@@ -172,7 +172,7 @@ const proyectos = [
   },
   {
     codigo: "01.",
-    coord: "00.0000000, 00.0000000",
+    coord: "36.4107, -6.2033",
     nombre: "Constelación Refugio",
     servicios: "Diagnóstico integrado",
     toolbox: "Cartografía",
