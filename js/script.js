1e0de076a033794c7a682bc6c97dba4eb5b6f77a
Maria <maria.moure.rosende@gmail.com>
2026-07-06 20:45:40 +0200

    tema

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const proyectos = [
   {
     codigo: "20.",
-    coord: "42.8782, -8.5448 ",
+    coord: "42.8782347, -8.5448265",
     nombre: "Compostela Xadrez",
+    tema: "Territorios",
     servicios: "Plataforma de datos",
     toolbox: "Cartografía, Desarrollo web",
     equipo: "",
@@ -10,8 +11,9 @@ const proyectos = [
   },
   {
     codigo: "19.",
-    coord: "41.3500, 2.0500",
+    coord: "41.3500184, 2.0500732",
     nombre: "Agenda Urbana del Baix Llobregat (AUBL)",
+    tema: "Agenda Urbana, Territorios",
     servicios: "Evaluación de políticas, Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Consell Comarcal Baix Llobregat, TECHfriendly",
@@ -19,8 +21,9 @@ const proyectos = [
   },
   {
     codigo: "18.",
-    coord: "39.1653, -0.2559",
+    coord: "39.1653271, -0.2559486",
     nombre: "Plan de Actuación Integrado Cullera",
+    tema: "Gobernanza, Regeneración urbana, Resiliencia urbana",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
     equipo: "Ajuntament de Cullera, TECHfriendly",
@@ -28,8 +31,9 @@ const proyectos = [
   },
   {
     codigo: "17.",
-    coord: "39.4913, -0.4635",
+    coord: "39.4913158, -0.4635374",
     nombre: "Plan de Actuación Integrado Manises",
+    tema: "Gobernanza, Regeneración urbana, Resiliencia urbana",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
     equipo: "Ajuntament de Manises, TECHfriendly",
@@ -37,8 +41,9 @@ const proyectos = [
   },
   {
     codigo: "16.",
-    coord: "41.4890, 2.3570",
+    coord: "41.4890635, 2.3570421",
     nombre: "Actualización Plan de Acción de Premià de Mar",
+    tema: "Gobernanza, Regeneración urbana, Resiliencia urbana",
     servicios: "Evaluación de políticas, Diagnóstico Integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Ajuntament de Premiá de Mar, TECHfriendly",
@@ -46,17 +51,19 @@ const proyectos = [
   },
   {
     codigo: "15.",
-    coord: "41.3436, 2.0419",
+    coord: "41.3436918, 2.0419536",
     nombre: "Pla de Barris Sant Boi de Llobregat",
+    tema: "Gobernanza, Regeneración urbana",
     servicios: "Candidatura programa de financiación",
-    toolbox: "",
+    toolbox: "Gobernanza",
     equipo: "Ajuntament de Sant Boi de Llobregat, TECHfriendly",
     img: ""
   },
   {
     codigo: "14.",
-    coord: "39.1930, -0.4360",
+    coord: "39.1930442, -0.4360189",
     nombre: "Agenda Urbana de Reconstrucción de Algemesí",
+    tema: "Agenda Urbana, Resiliencia urbana, Territorios",
     servicios: "Evaluación de políticas, Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Ajuntament de Algemesí, TECHfriendly",
@@ -64,8 +71,9 @@ const proyectos = [
   },
   {
     codigo: "13.",
-    coord: "43.5560, -5.9248",
+    coord: "43.5560276, -5.9248831",
     nombre: "Plan de Actuación Integrado Avilés",
+    tema: "Gobernanza, Regeneración urbana",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
     equipo: "Ayuntamiento de Avilés, TECHfriendly",
@@ -73,8 +81,9 @@ const proyectos = [
   },
   {
     codigo: "12.",
-    coord: "41.4926, 2.0312",
+    coord: "41.4926549, 2.0312287",
     nombre: "Plan de Actuación Integrado Rubí",
+    tema: "Gobernanza, Regeneración urbana",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
     equipo: "Ajuntament de Rubí, TECHfriendly",
@@ -82,8 +91,9 @@ const proyectos = [
   },
   {
     codigo: "11.",
-    coord: "41.3030, 2.0012",
+    coord: "41.3030394, 2.0012768",
     nombre: "Plan de Actuación Integrado Gavà",
+    tema: "Gobernanza, Regeneración urbana",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
     equipo: "Ajuntament de Gavà, TECHfriendly",
@@ -91,8 +101,9 @@ const proyectos = [
   },
   {
     codigo: "10.",
-    coord: "42.5618, -8.9915",
+    coord: "42.5618785, -8.9915432",
     nombre: "Plan de Actuación Integrado Ribeira",
+    tema: "Gobernanza, Regeneración urbana",
     servicios: "Candidatura programa de financiación",
     toolbox: "",
     equipo: "Concello de Ribeira, TECHfriendly",
@@ -100,8 +111,9 @@ const proyectos = [
   },
   {
     codigo: "09.",
-    coord: "6.7213, -4.4214",
+    coord: "6.7213468, -4.4214917",
     nombre: "Actualización Diagnóstico AU Málaga",
+    tema: "Agenda Urbana, Territorios",
     servicios: "Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Ayuntamiento de Málaga, TECHfriendly",
@@ -109,8 +121,9 @@ const proyectos = [
   },
   {
     codigo: "08.",
-    coord: "41.3500, 2.0500",
+    coord: "41.3500267, 2.0500948",
     nombre: "PAC Baix Llobregat 2024-2027",
+    tema: "Agenda Urbana, Territorios",
     servicios: "Evaluación de políticas, Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Consell Comarcal Baix Llobregat, TECHfriendly",
@@ -118,8 +131,9 @@ const proyectos = [
   },
   {
     codigo: "07.",
-    coord: "41.4500, 2.2474",
+    coord: "41.4500521, 2.2474865",
     nombre: "Badalona Estrategia 2030",
+    tema: "Agenda Urbana, Territorios",
     servicios: "Evaluación de políticas, Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Ajuntament de Badalona, TECHfriendly",
@@ -127,8 +141,9 @@ const proyectos = [
   },
   {
     codigo: "06.",
-    coord: "41.3500, 2.0500",
+    coord: "41.3500267, 2.0500948",
     nombre: "Estudio turismo azul costa Barcelona",
+    tema: "Territorios, Turismo",
     servicios: "Evaluación de políticas, Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Diputació de Barcelona, TECHfriendly",
@@ -136,8 +151,9 @@ const proyectos = [
   },
   {
     codigo: "05.",
-    coord: "42.5126, -8.8129",
+    coord: "42.5126813, -8.8129375",
     nombre: "Palimpsesto Atlántico",
+    tema: "Territorios",
     servicios: "Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "",
@@ -145,8 +161,9 @@ const proyectos = [
   },
   {
     codigo: "04.",
-    coord: "41.3851, 2.1734",
+    coord: "41.3851492, 2.1734617",
     nombre: "Análisis de la conectividad ecológica",
+    tema: "Territorios",
     servicios: "Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Àrea Metropolitana de Barcelona",
@@ -154,8 +171,9 @@ const proyectos = [
   },
   {
     codigo: "03.",
-    coord: "41.3851, 2.1734",
+    coord: "41.3851738, 2.1734284",
     nombre: "Prioridades Invertidas",
+    tema: "Movilidad, Territorios",
     servicios: "Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "Àrea Metropolitana de Barcelona",
@@ -163,8 +181,9 @@ const proyectos = [
   },
   {
     codigo: "02.",
-    coord: "42.7241, -7.6833",
+    coord: "42.7241695, -7.6833872",
     nombre: "Gestión del agua",
+    tema: "Agua, Territorios",
     servicios: "Diagnóstico integrado",
     toolbox: "Análisis de datos, Cartografía",
     equipo: "",
@@ -172,8 +191,9 @@ const proyectos = [
   },
   {
     codigo: "01.",
-    coord: "36.4107, -6.2033",
+    coord: "36.4107581, -6.2033496",
     nombre: "Constelación Refugio",
+    tema: "Territorios",
     servicios: "Diagnóstico integrado",
     toolbox: "Cartografía",
     equipo: "",
@@ -231,11 +251,21 @@ proyectos.forEach(p => {
 
   fila.dataset.toolbox = p.toolbox || "";
 
+const temaHTML = p.tema
+  ? p.tema
+      .split(",")
+      .map(t => `<span class="tema-item">${t.trim()}</span>`)
+      .join(", ")
+  : "";
+
+fila.dataset.tema = p.tema || "";
+
+
   /* ===== HTML ===== */
   fila.innerHTML = `
-    <div><p>${p.codigo}</p></div>
     <div><p>${p.coord}</p></div>
     <div><p>${p.nombre}</p></div>
+    <div><p>${temaHTML}</p></div>
     <div><p>${serviciosHTML}</p></div> 
     <div><p>${toolboxHTML}</p></div>
     <div><p>${p.equipo}</p></div>
@@ -273,6 +303,15 @@ proyectos.forEach(p => {
       filtrar("toolbox", valor);
     });
   });
+
+  /* ===== EVENTOS TEMA ===== */
+fila.querySelectorAll(".tema-item").forEach(item => {
+  item.addEventListener("click", (e) => {
+    e.stopPropagation();
+    const valor = item.textContent.trim();
+    filtrar("tema", valor);
+  });
+});
 
   /* ===== INSERTAR FILA ===== */
   tabla.appendChild(fila);
